67a1e429385f3c2e916504a095825749f784c775
cruel32 <cshee8508@gmail.com>
2019-05-12 22:49:53 +0900

    react native 테스트

--- native-project/App.js
+++ native-project/App.js
@@ -5,7 +5,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!!</Text>
+        <Text>Open up App.js to start working on your app</Text>
+        <Text>안드로이드폰으로 리액트 네이티브 프로젝트</Text>
       </View>
     );
   }

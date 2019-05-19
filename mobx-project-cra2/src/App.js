50819931688b9cfde095773dce5c2b95b999233d
cruel-32 <cruel32@nate.com>
2019-05-19 12:24:59 +0900

    mobx project cra 2.0 ->3.0

---
+++ mobx-project-cra2/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Home from './pages/home';
+import Counter from './pages/counter';
+import SuperMarket from './pages/super_market';
+import DevTools from 'mobx-react-devtools';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Link to={`/`}>Home</Link>
+          <Link to={`/SuperMarket/1000?items=2`}>Super Market</Link>
+          <Link to={`/Counter/2000?count=10`}>Counter</Link>
+
+          {process.env.NODE_ENV === 'development' && <DevTools />}
+
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/SuperMarket/:id" component={SuperMarket}/>
+          <Route exact path="/Counter/:id" component={Counter}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- mobx-project/src/index.js
+++ mobx-project/src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
-import './index.scss';
+import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-import RootStore from './stores';
+import * as serviceWorker from './serviceWorker';
 
-const root = new RootStore(); // *** 루트 스토어 생성
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-  <Provider {...root}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
-// registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
